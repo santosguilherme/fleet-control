14b471d7d94ad7cd7f51c121a4986c0e1c157819
Guilherme Santos <sntguilherme@gmail.com>
2017-04-11 01:52:13 -0300

    build files fix

--- gulp/build.js
+++ gulp/build.js
@@ -82,7 +82,6 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-
   return gulp.src($.mainBowerFiles())
 
     .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
@@ -97,7 +96,7 @@ gulp.task('other', function () {
 
   return gulp.src([
     path.join(conf.paths.src, '/**/*'),
-    path.join('!' + conf.paths.src, '/**/*.{html,css,js}')
+    path.join('!' + conf.paths.src, '/**/*.{html,css,js,scss}')
   ])
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
